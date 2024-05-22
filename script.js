6abbee9b982909a1ee34ab5ee4fba796cb9b428d
satyasuresh <ssch16c@gmail.com>
2024-05-22 22:31:35 +0530

    Updated

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+=======
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+>>>>>>> 004df67 (Updated)
 }
